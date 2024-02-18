870bba51890c0dddafd573d2ac920c4c3c96ee7c
yoe21c <yoe21c@gmail.com>
2024-02-18 20:36:18 +0900

    * fix

--- src/main/java/byeon/task20240213/DataController.java
+++ src/main/java/byeon/task20240213/DataController.java
@@ -2,24 +2,37 @@ package byeon.task20240213;
 
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class DataController {
+
+    private final Environment environment;
+
+    @Value("${mode}")
+    private String mode;
 
     @GetMapping("/data")
     public Form data() {
-        return new Form(new Data("ok"));
+        log.debug("/data mode: {}", mode);
+        log.info("/data mode: {}", mode);
+        return new Form(new Data("ok", getActiveProfile()));
     }
 
 
     @GetMapping("/data2")
     public ResponseEntity<Form> data2() {
-        return new ResponseEntity<>(new Form(new Data("ok")), HttpStatus.OK);
+        return new ResponseEntity<>(new Form(new Data("ok", getActiveProfile())), HttpStatus.OK);
     }
 
     @Getter
@@ -27,9 +40,11 @@ public class DataController {
     static class Data {
 
         private String result;
+        private String profile;
 
-        public Data(String result) {
+        public Data(String result, String profile) {
             this.result = result;
+            this.profile = profile;
         }
     }
 
@@ -42,5 +57,16 @@ public class DataController {
             this.data = data;
         }
 
+    }
+
+    private String getActiveProfile() {
+
+        // 첫번째 것만 리턴하자.
+        String profile = "";
+        for (String activeProfile : environment.getActiveProfiles()) {
+            profile = activeProfile;
+            break;
+        }
+        return profile;
     }
 }
